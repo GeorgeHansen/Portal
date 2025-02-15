b091a5b077d909adcaa704a06e60efe656fb332b
Nicholas Hansen <nicholasfhansen@gmail.com>
2025-02-15 17:44:25 +0100

    Add temporary navbar

--- components/AppLayout/AppLayout.tsx
+++ components/AppLayout/AppLayout.tsx
@@ -1,7 +1,9 @@
 'use client';
 
-import { AppShell, Burger } from '@mantine/core';
+import { AppShell, Burger, Button, Divider, Flex, Group, NavLink, Stack } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
+import { Image } from '@mantine/core';
+import { IconArrowsRightLeft, IconChevronRight, IconGauge, IconHome2, IconLogout } from '@tabler/icons-react';
 
 export default function AppLayout({children} : {children:React.ReactNode}) {
   const [opened, { toggle }] = useDisclosure();
@@ -16,17 +18,61 @@ export default function AppLayout({children} : {children:React.ReactNode}) {
       }}
       padding="md"
     >
+     
       <AppShell.Header>
-        <Burger
-          opened={opened}
-          onClick={toggle}
-          hiddenFrom="sm"
-          size="sm"
-        />
-        <div>Logo</div>
+      <Flex 
+        justify="flex-start"
+        align="center"
+        h="100%"
+        gap="sm"
+      >
+          <Burger
+            opened={opened}
+            onClick={toggle}
+            hiddenFrom="sm"
+            size="sm"
+          />
+          <Image src="/Portal_logo.svg.png" h="40" w="auto" style={{marginLeft:"10px"}} />
+        </Flex>
       </AppShell.Header>
 
-      <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+      <AppShell.Navbar p="md" >
+          <Stack justify="flex-start" h="100%">
+          <NavLink
+            href="#required-for-focus"
+            label="With icon"
+            leftSection={<IconHome2 size={24} stroke={1.5} />}
+          />
+           <NavLink
+            href="#required-for-focus"
+            label="With right section"
+            leftSection={<IconGauge size={24} stroke={1.5} />}
+            rightSection={
+              <IconChevronRight size={16} stroke={1.5} className="mantine-rotate-rtl" />
+            }
+          >
+            <NavLink href="#required-for-focus" label="First child link" />
+          </NavLink>
+          </Stack>
+          <Stack 
+            justify="flex-end"
+            h="250"
+            gap="sm"
+          >
+          <Divider my="sm" /> 
+            <NavLink
+            href="#required-for-focus"
+            label="Change Application"
+            leftSection={<IconArrowsRightLeft size={24} stroke={1.5} />}
+            />
+            <NavLink
+            href="#required-for-focus"
+            label="Logout"
+            leftSection={<IconLogout size={24} stroke={1.5} />}
+            />
+          </Stack>
+        
+      </AppShell.Navbar>
 
       <AppShell.Main>{children}</AppShell.Main>
     </AppShell>
